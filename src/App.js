dcf403e08cf9d75cc40c67e9c271b8f80fb76ee8
Illia Smolii <illiasmolii@gmail.com>
2017-12-05 18:33:52 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class App extends React.Component {
+
+  render() {
+    return (
+      <div className="root-container">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
