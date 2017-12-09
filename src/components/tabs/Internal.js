95d4b6a92cfa422916c35c44bfb8db23e1ff5625
Illia Smolii <illia.smolii@sysgears.com>
2017-12-09 13:14:09 +0200

    Added basic form for internal payment.

--- src/components/tabs/Internal.js
+++ src/components/tabs/Internal.js
@@ -1,10 +1,47 @@
 import React from "react";
 
+import Select from "../common/Select";
+
 export default class Internal extends React.Component {
 
   render() {
     return (
-      <h1>Internal payment form will be here</h1>
+      // TODO add Bootstrap
+      <section>
+        <h1>Internal payment</h1>
+        <form>
+          <label>
+            Name
+            <input type={'text'}/>
+          </label>
+          <br/>
+          <label>
+            IBAN
+            <input type={'text'}/>
+          </label>
+          <br/>
+          <Select label={'Type of the payment'}
+                  options={
+                    [
+                      {
+                        name: 'Regular',
+                        value: 'regular'
+                      },
+                      {
+                        name: 'Charity',
+                        value: 'charity'
+                      }
+                    ]
+                  }/>
+          <br/>
+          <label>
+            Amount
+            <input type={'text'}/>
+          </label>
+          <br/>
+          <input type={'submit'} value={'Create payment'}/>
+        </form>
+      </section>
     );
   }
 }
