a26b9b9574c98949619f1b39fa00bda48ac5264b
Illia Smolii <illiasmolii@gmail.com>
2017-12-13 18:40:01 +0200

    Table alignment.

--- src/components/tabs/my-payments/MyPayments.js
+++ src/components/tabs/my-payments/MyPayments.js
@@ -7,24 +7,26 @@ export default class MyPayments extends React.Component {
   render() {
     if (this.props.payments) {
       return (
-        <section>
-          <table className={'table'}>
-            <tbody>
-            <tr>
-              <th>#</th>
-              <th>Recipient</th>
-              <th>Date</th>
-              <th>Currency</th>
-              <th>Amount</th>
-              <th>Details</th>
-            </tr>
-            {
-              this.props.payments.map((p, index) =>
-                <Payment key={index} i={index + 1} payment={p}/>
-              )
-            }
-            </tbody>
-          </table>
+        <section className={'row'}>
+          <div className={'col-md-8 col-md-offset-2'}>
+            <table className={'table'}>
+              <tbody>
+              <tr>
+                <th>#</th>
+                <th>Recipient</th>
+                <th>Date</th>
+                <th>Currency</th>
+                <th>Amount</th>
+                <th>Details</th>
+              </tr>
+              {
+                this.props.payments.map((p, index) =>
+                  <Payment key={index} i={index + 1} payment={p}/>
+                )
+              }
+              </tbody>
+            </table>
+          </div>
         </section>
       );
     } else {
